49767fb39e0338546e7800200dfdce0be8467ebe
indianlyc <kiselevpg@gmail.com>
2023-09-09 10:30:51 +0500

    230909

---
+++ 20230909/main.js
@@ -0,0 +1,15 @@
+function f1(){
+    document.getElementById("base_text").innerHTML = "Первый текст"
+}
+
+function f2(){
+    document.getElementById("base_text").innerHTML = "Второй текст"
+}
+
+function f3(){
+    document.getElementById("base_text").innerHTML = "Первый текст"
+}
+
+function f4(){
+    document.getElementById("base_text").innerHTML = "Первый текст"
+}
