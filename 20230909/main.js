b5651bf5806c8bb74954b15f84a687145a55a78a
indianlyc <kiselevpg@gmail.com>
2023-09-09 13:33:44 +0500

    230909

--- 20230909/main.js
+++ 20230909/main.js
@@ -1,15 +1,28 @@
-function f1(){
-    document.getElementById("base_text").innerHTML = "Первый текст"
+/*
+let fruits = {
+    "u1": "Первый текст",
+    "u2": "Второй текст",
+    "u3": "Третий текст",
+    "u4": "Четвертый текст <img class=\"dog\" src=\"https://fanibani.ru/wp-content/uploads/2022/12/1644866254_20-fikiwiki-com-p-shchenki-krasivie-kartinki-22.jpg\">"
+    };
+*/
+
+function g(el){
+    function f(){
+            document.getElementById("base_text").innerHTML = el.getElementsByClassName("text")[0].innerHTML;
+    };
+    return f;
 }
 
-function f2(){
-    document.getElementById("base_text").innerHTML = "Второй текст"
+
+function main(){
+    let y = document.getElementsByClassName('u');
+    for(let i=0; i < y.length; i++){
+        y[i].onclick = g(y[i]);
+    };
+
 }
 
-function f3(){
-    document.getElementById("base_text").innerHTML = "Первый текст"
-}
+document.addEventListener("DOMContentLoaded", main);
 
-function f4(){
-    document.getElementById("base_text").innerHTML = "Первый текст"
-}
+
