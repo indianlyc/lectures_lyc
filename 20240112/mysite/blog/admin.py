1e776c2f934388263aa7094088705b2c8d810a85
indianlyc <kiselevpg@gmail.com>
2024-01-19 14:28:37 +0500

    add lecture

--- 20240112/mysite/blog/admin.py
+++ 20240112/mysite/blog/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "datetime"
+    list_display = ["datetime", "title"]
+
+# admin.site.register(Post, PostAdmin)
--- 20240112/mysite/blog/views.py
+++ 20240112/mysite/blog/views.py
@@ -1,8 +1,23 @@
 # from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Post
 
 
 def index(request):
-    return HttpResponse("Привет! Это страница моего блога")
-#
+    latest_post_list = Post.objects.order_by("-datetime")
+    # output = ", ".join([q.title for q in latest_post_list])
+    # return HttpResponse(output)
+
+    template = loader.get_template("blog/index.html")
+    context = {
+        "latest_post_list": latest_post_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    output = post.text
+    return HttpResponse(output)
