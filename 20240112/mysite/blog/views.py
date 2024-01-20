a6c59aef33099d8ee69afc8fe3d2735b42605d90
indianlyc <kiselevpg@gmail.com>
2024-01-20 12:33:07 +0500

    add lecture

--- 20240112/mysite/blog/views.py
+++ 20240112/mysite/blog/views.py
@@ -1,7 +1,8 @@
-# from django.shortcuts import render
-
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
 
 from .models import Post
 
@@ -11,13 +12,33 @@ def index(request):
     # output = ", ".join([q.title for q in latest_post_list])
     # return HttpResponse(output)
 
-    template = loader.get_template("blog/index.html")
+    # template = loader.get_template("blog/index.html")
     context = {
         "latest_post_list": latest_post_list,
     }
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, "blog/index.html", context)
+
+
+# def detail(request, post_id):
+#     # try:
+#     #     post = Post.objects.get(pk=post_id)
+#     # except Post.DoesNotExist:
+#     #     raise Http404("Такого поста нет")
+#     post = get_object_or_404(Post, pk=post_id)
+#     # template = loader.get_template("blog/post.html")
+#     context = {
+#         "post": post,
+#         "post_id": post_id,
+#     }
+#     #  return HttpResponse(template.render(context, request))
+#     return render(request, "blog/post.html", context)
 
 def detail(request, post_id):
-    post = Post.objects.get(pk=post_id)
-    output = post.text
-    return HttpResponse(output)
+    latest_post_list = Post.objects.order_by("-datetime")
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        "post": post,
+        "latest_post_list": latest_post_list,
+    }
+    return render(request, "blog/post.html", context)
--- 20240112/mysite/mysite/settings.py
+++ 20240112/mysite/mysite/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = '/home/indianlyc/Документы/projects/liceum/lecture_lyc/20240112/mysite/blog/static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
