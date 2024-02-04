31c92276ea32ad1e6568de4fe7e80439968c4768
indianlyc <kiselevpg@gmail.com>
2024-02-04 11:35:58 +0500

    add lecture and dz

--- 20240112/mysite/blog/views.py
+++ 20240112/mysite/blog/views.py
@@ -2,10 +2,15 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
 
 
 from .models import Post
-
+from comments.forms import CommentForm
+from comments.models import Comment
 
 def index(request):
     latest_post_list = Post.objects.order_by("-datetime")
@@ -37,8 +42,25 @@ def index(request):
 def detail(request, post_id):
     latest_post_list = Post.objects.order_by("-datetime")
     post = get_object_or_404(Post, pk=post_id)
+    comment_list = Comment.objects.filter(post=post).order_by("datetime")
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            text = form.cleaned_data["text"]
+            comment = Comment.objects.create(post=post,
+                                             username=username,
+                                             text=text)
+            comment.save()
+            return HttpResponseRedirect(reverse("detail", args=(post_id,)))
+    else:
+        form = CommentForm()
+
     context = {
+        "comments": comment_list,
         "post": post,
+        "form": form,
         "latest_post_list": latest_post_list,
     }
     return render(request, "blog/post.html", context)
--- 20240112/mysite/comments/models.py
+++ 20240112/mysite/comments/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Comment(models.Model):
+    post = models.ForeignKey("blog.Post",
+                             on_delete=models.CASCADE,
+                             # blank=True,
+                             # null=True
+                             )
     username = models.CharField("Ник", max_length=20)
     text = models.TextField("Текст")
     datetime = models.DateTimeField("Дата и время", auto_now_add=True)
